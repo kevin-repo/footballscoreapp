c3eef55ad2512a46671321b4577745a77ac1f9ab
Kévin Quinton <kevin.quinton@cgi.com>
2019-03-19 11:02:41 +0100

    add of matches component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatchesComponent } from './pages/matches/matches.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchesComponent
   ],
   imports: [
     BrowserModule,
